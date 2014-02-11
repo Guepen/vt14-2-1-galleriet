eabe64df21abcc271e3359f27b644d67b5e3c258
th222fa <th222fa@student.lnu.se>
2014-02-12 00:43:14 +0100

    page setup och nästan gjort klart min klass

---
+++ Galleriet/Galleriet/Default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Galleriet.Default" ViewStateMode="Disabled" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Repeater ID="Repeater1" runat="server">
+                <HeaderTemplate>
+                    <ul class="files">
+                </HeaderTemplate>
+                <ItemTemplate>
+                    <li>
+                        <asp:HyperLink ID="FileyperLink" runat="server" NavigateUrl="">HyperLink</asp:HyperLink>
+                    </li>
+                </ItemTemplate>
+            </asp:Repeater>
+        </div>
+        
+        <%-- Validering  --%>
+        <div>
+           
+        </div>
+
+        <%-- Upladdningen av fil  --%>
+    <div>
+        <asp:FileUpload ID="FileUpload" runat="server" />
+        <asp:Button ID="ButtonUpload" runat="server" Text="Ladda Upp" OnClick="ButtonUpload_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Galleriet/Galleriet/Model/Gallery.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Drawing.Imaging;
+
+namespace Galleriet.Model
+{
+    public class Gallery
+    {
+        private static readonly Regex ApprovedExtensions;
+        private static string PhysicalUploadedImagesPath;
+        private static readonly Regex SantizePath;
+
+        static Gallery()
+        {
+            ApprovedExtensions = new Regex("^.*.(gif|jpg|png)$", RegexOptions.IgnoreCase);
+
+            PhysicalUploadedImagesPath = Path.Combine(AppDomain.CurrentDomain.GetData("APPBASE").ToString(), "Content/Images");
+
+            var invalidChars = new string(Path.GetInvalidFileNameChars());
+            SantizePath = new Regex(string.Format("[{0}]", Regex.Escape(invalidChars)));
+        }
+        
+
+        public IEnumerable<string> GetImageNames()
+        {
+            var sortedFiles = new DirectoryInfo(PhysicalUploadedImagesPath).GetFiles().OrderBy(f => f.Name).ToList();
+
+            return sortedFiles;
+        }
+
+        public bool ImageExists(string name)
+        {
+            if (GetImageNames().Contains(name))
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+		
+        }
+
+        private bool IsValidImage(Image image)
+        {
+            if (image.RawFormat.Guid == System.Drawing.Imaging.Gif.Guid
+                || image.RawFormat.Guid == System.Drawing.Imaging.Jpeg.Guid 
+                || image.RawFormat.Guid == System.Drawing.Imaging.Png.Guid)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false; 
+            }
+        }
+
+        public string SaveImage(Stream stream, string fileName)
+        { }
+    }
+}
